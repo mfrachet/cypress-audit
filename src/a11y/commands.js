f8678bb3b9b112b1e2008270bad09e72e6d1f7c0
Marvin Frachet <marvin@gatsbyjs.com>
2020-05-22 14:28:44 +0200

    Safe pa11y run when browser is not supported (#18)

--- src/a11y/commands.js
+++ src/a11y/commands.js
@@ -1,5 +1,11 @@
 const ERROR_NUMBER_THRESHOLD = 0;
 
+const VALID_BROWSERS = {
+  Chrome: true,
+  Chromium: true,
+  Canary: true,
+};
+
 const groupIssues = (issues) => {
   const groupedIssuesDict = issues.reduce(
     (allIssues, { code, message, runnerExtras }) => {
@@ -16,7 +22,7 @@ const groupIssues = (issues) => {
       }
       return allIssues;
     },
-    {},
+    {}
   );
 
   const groupedIssues = [];
@@ -34,21 +40,29 @@ const formatIssues = (issues) => {
   return issues
     .map(
       (issue) =>
-        `Issue: ${issue.issueId}, # of occurrences: ${issue.occurrences}.\n- ${issue.description}\n- ${issue.link}`,
+        `Issue: ${issue.issueId}, # of occurrences: ${issue.occurrences}.\n- ${issue.description}\n- ${issue.link}`
     )
     .join(`\n\n`);
 };
 
 Cypress.Commands.add("pa11y", (opts) => {
+  if (!VALID_BROWSERS[Cypress.browser.displayName]) {
+    return cy.log(
+      "cy.pa11y()",
+      `${Cypress.browser.displayName} is not supported. Skipping...`
+    );
+  }
+
   cy.url()
     .then((url) => cy.task("pa11y", { url, opts }))
     .then((issues) => {
       if (issues.length > ERROR_NUMBER_THRESHOLD) {
         const groupedIssues = groupIssues(issues);
 
-        const title = issues.length === 1
-          ? `cy.pa11y - ${issues.length} accessibility violation was found`
-          : `cy.pa11y - ${issues.length} accessibility violations were found`;
+        const title =
+          issues.length === 1
+            ? `cy.pa11y - ${issues.length} accessibility violation was found`
+            : `cy.pa11y - ${issues.length} accessibility violations were found`;
 
         const formattedIssues = formatIssues(groupedIssues);
 
