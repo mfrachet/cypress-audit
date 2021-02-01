573ed6bd89e9656550f383b408f6c83909bbfce6
mfrachet <marvin.frachet@strapi.io>
2021-02-01 18:23:10 +0100

    Attempt to bump

--- examples/external-url/cypress/integration/main.spec.js
+++ examples/external-url/cypress/integration/main.spec.js
@@ -1,6 +1,12 @@
-context("External", () => {
+describe("External", () => {
   it("check scores", () => {
     cy.visit("https://google.com/");
-    cy.lighthouse();
+
+    cy.lighthouse({
+      accessibility: 90,
+      "best-practices": 50,
+      seo: 50,
+      pwa: 50,
+    });
   });
 });
