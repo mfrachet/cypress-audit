85bec3802dd8f1491df6afb8f95b5c8c1d17a9f6
mfrachet <undefined>
2023-03-22 07:34:59 +0100

    refactor(deps): pnpm workspaces

---
+++ examples/nextjs/next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // Webpack 5 is enabled by default
+  // You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
+  webpack5: false,
+};
